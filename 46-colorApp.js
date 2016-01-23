5838608f7071f6f646c6447ef398e4db382de8bc
Nate <long3000@gmail.com>
2016-01-23 12:14:55 -0800

    Redo the randomColorGenerator code and clean up the functions

--- 46-colorApp.js
+++ 46-colorApp.js
@@ -1,35 +1,45 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(255, 0, 255)",
-]
+var colors = [];
+var numSquares = 6;
+var colorDisplay = document.querySelector("#colorDisplay");
+var pickedColor;
+var resetBtn = document.querySelector("#new");
+var squares = document.querySelectorAll(".sq");
+
+//Initialize all prep functions
+init();
+
+function init() {
+	reset();
+}
 
 
-var square = document.querySelectorAll(".sq");
+//Push random colors into color array
+function randomColorGenerator(num) {
+	var colorArr = []
+	for(i = 0; i < num; i++) {
+		colorArr.push(randomColors())
+	}
+	return colorArr;
+}
 
-var pickColor = colors[3];
-var displayText = document.getElementById("pick");
+function reset() {
+colors = randomColorGenerator(numSquares);
+for(i = 0 ; i < squares.length; i++) {
+	squares[i].style.background = colors[i];
+	}
+}
 
+function pickColor() {
+	var random  = Math.floor(Math.random()* squares.length)
+	return colors[random];
+}
 
-displayText.textContent = pickColor;
-
-for(i = 0 ; i < square.length ; i++) {
-	//Add initial colors to the squares
-	square[i].style.background = colors[i];
-	//Listening event for squares
-	square[i].addEventListener("click",function(){
-		//grab color
-		var clickedColor = this.style.background;
-		//compare the color
-		if(clickedColor === pickColor){
-			alert("Correct");
-		//square[i].style.background = #232323;
-		} else {
-			alert("Wrong");
-		//this.style.background = #232323;
-		}
-	});
+//Generate random color
+function randomColors() {
+	//random RED, GREEN, BLUE using 255 scale
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	//Return correct RGB format
+	return "rgb("+r+", "+g+", "+b+")";
 }
