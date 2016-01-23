52f2971e8b48a08ae4226514b6bc8bf248118657
Nate <long3000@gmail.com>
2016-01-23 13:58:59 -0800

    Adding mode buttons to display different square arrays

--- 46-colorApp.js
+++ 46-colorApp.js
@@ -1,15 +1,43 @@
 var colors = [];
-var numSquares = 6;
+var numSquares 6;
 var colorDisplay = document.querySelector("#colorDisplay");
 var pickedColor;
 var resetBtn = document.querySelector("#new");
+var easyBtn - document.querySelector("#easyBtn");
+var hardBtn - document.querySelector("#hardBtn");
 var squares = document.querySelectorAll(".sq");
+var messageDisplay = document.querySelector("#messageDisplay");
+var modeBtn = document.querySelectorAll(".mode");
 
 //Initialize all prep functions
 init();
-
 function init() {
+	initSquares();
 	reset();
+}
+
+
+function setupMode() {
+	for(var i = 0; i < modeBtn.length; i++) {
+
+	}
+}
+
+function initSquares() {
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].addEventListener("click", function() {
+			//match the color of the clicked color to a variable
+			var clickedColor = this.style.background;
+			if(clickedColor === pickedColor){
+				messageDisplay.textContent = "Correct!";
+				changeColors(pickedColor);
+			} else {
+				messageDisplay.textContent = "Try again.";
+				this.style.background = "#232323";
+			}
+		});
+	}
+
 }
 
 
@@ -22,8 +50,18 @@ function randomColorGenerator(num) {
 	return colorArr;
 }
 
+function changeColors (color){
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.background = color;
+	}
+}
+
 function reset() {
 colors = randomColorGenerator(numSquares);
+//Set pickedColor to pickColor function
+pickedColor = pickColor();
+//Let the RGB value displayed out using colorDisplay
+colorDisplay.textContent = pickedColor;
 for(i = 0 ; i < squares.length; i++) {
 	squares[i].style.background = colors[i];
 	}
